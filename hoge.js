338c149f56e054552cd28e42fc8d38c3221b2264
yura7gi14 <yura7gi14@github.com>
2023-02-12 00:08:00 +0900

    強硬策で取得

--- hoge.js
+++ hoge.js
@@ -1,7 +1,25 @@
 
-// 先程生成したURL
+// 生成したURL
 const url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1050436635711957152';
 
+// 書き出し
+const updateText = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        const insertHtml = `
+            <li>
+                <a href="${data[i].recipeUrl}" target="_blank">
+                    <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
+                </a>
+                <h2>${data[i].recipeTitle}</h2>
+                <p>${data[i].recipeDescription}</p>
+            </li>
+        `;
+        $('#recipe_list').append(insertHtml);
+    }
+}
+
+// API取得
 $.getJSON(url, (data) => {
-    console.log(data.result);
+    const recipes = data.result;
+    updateText(recipes);
 });
