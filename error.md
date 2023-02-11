5c15ee7ced91add353938524361bbb9740daba7f
yura7gi14 <yura7gi14@github.com>
2023-02-11 20:49:32 +0900

    API検証プログラムの更新とエラー分岐

---
+++ error.md
@@ -0,0 +1,12 @@
+# エラーの状態
+## KeyError
+
+## Empty Dataframe
+pprint(df_recipe)
+
+## カテゴリーの分類
+pprint(df)
+
+## ランキングのすべて
+pprint(json_data)
+pprint(recipes)
--- hoge.py
+++ hoge.py
@@ -16,30 +16,32 @@ parent_dict = {} # mediumカテゴリの親カテゴリの辞書
 df = pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName'])
 
 for category in json_data['result']['large']:
-    df = df.concat([df,pd.DataFrame({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
+    df = pd.concat([df, pd.DataFrame({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
 
 for category in json_data['result']['medium']:
-    df = df.concat([df,pd.DataFrame({'category1':category['parentCategoryId'],'category2':category['categoryId'],'category3':"",'categoryId':str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
+    df = pd.concat([df, pd.DataFrame({'category1':category['parentCategoryId'],'category2':category['categoryId'],'category3':"",'categoryId':str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
     parent_dict[str(category['categoryId'])] = category['parentCategoryId']
 
 for category in json_data['result']['small']:
-    df = df.concat([df,pd.DateFrame({'category1':parent_dict[category['parentCategoryId']],'category2':category['parentCategoryId'],'category3':category['categoryId'],'categoryId':parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
+    df = pd.concat([df,pd.DataFrame({'category1':parent_dict[category['parentCategoryId']],'category2':category['parentCategoryId'],'category3':category['categoryId'],'categoryId':parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, index=[0])], ignore_index=True)
 
-# 2. キーワードからカテゴリを抽出する
-df_keyword = df.query('categoryName.str.contains("魚")', engine='python')
+# キーワードを含む行を抽出
+df_keyword = df.query('categoryName.str.contains("肉")', engine='python')
 
-# 3. 人気レシピを取得する
-df_recipe = pd.DataFrame(columns=['recipeId', 'recipeTitle', 'foodImageUrl', 'recipeMaterial', 'recipeCost', 'recipeIndication', 'categoryId', 'categoryName'])
+# 人気レシピを取得
+res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1050436635711957152')
 
-for index, row in df_keyword.iterrows():
-    time.sleep(3) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
+df_recipe = pd.DataFrame(columns=['recipeId', 'recipeTitle', 'foodImageUrl',  'categoryId', 'categoryName'])
 
-    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1050436635711957152&categoryId='+row['categoryId']
-    res = requests.get(url)
+#for index, row in df_keyword.iterrows():
+#    time.sleep(3) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
+#
+#    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1050436635711957152&categoryId='+row['categoryId']
+#    res = requests.get(url)
+#
+#    json_data = json.loads(res.text)
+#    recipes = json_data['result']
 
-    json_data = json.loads(res.text)
-    recipes = json_data['result']
+json_data = json.loads(res.text)
 
-    for recipe in recipes:
-        df_recipe = df_recipe.concat([df,pd.DateFrame({'recipeId':recipe['recipeId'],'recipeTitle':recipe['recipeTitle'],'foodImageUrl':recipe['foodImageUrl'],'recipeMaterial':recipe['recipeMaterial'],'recipeCost':recipe['recipeCost'],'recipeIndication':recipe['recipeIndication'],'categoryId':row['categoryId'],'categoryName':row['categoryName']}, index=[0])], ignore_index=True)
-        pprint(df_recipe)
+pprint(json_data)
